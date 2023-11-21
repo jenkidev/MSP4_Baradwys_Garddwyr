71887c0493701008b8894de8d164193dbba05ec5
jenkidev <>
2023-11-21 15:06:22 +0000

    Started django app

---
+++ baradwys_garddwyr/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for baradwys_garddwyr project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baradwys_garddwyr.settings')
+
+application = get_asgi_application()
