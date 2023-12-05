16d1eea41f87960dabd947553d618fe5ee81c0fd
jenkidev <>
2023-12-05 04:55:58 +0000

    Added article views and templates

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def all_articles(request):
+    """ A view to return all articles """
+
+    articles = Article.objects.all()
+
+    context = {
+        'articles': articles 
+    }
+
+    return render(request, 'articles/articles.html', context)
