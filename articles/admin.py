44ec6c8795a6ea986f928759482c72883fc88f46
jenkidev <>
2023-12-05 04:28:13 +0000

    created article model in database

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'blurb',
+        'content',
+        'rating',
+        'image',
+    )
+
+    ordering = ('pk',)
+
+
+admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=254)
+    blurb = models.CharField(max_length=508)
+    content = models.TextField()
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
