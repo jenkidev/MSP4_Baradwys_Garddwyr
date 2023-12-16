0210d74a81a2c8bb5b0b651dd46035543258b809
jenkidev <>
2023-12-16 20:51:57 +0000

    reated testing for products

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
-# Create your tests here.
+from products.models import Product, Category
+
+
+class TestViews(TestCase):
+
+    def test_see_all_products(self):
+        """test view to see all products"""
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+
+        template = 'products/products.html'
+        self.assertTemplateUsed(response, template)
+
+    def test_see_product_details(self):
+        """test view to see product details"""
+
+        category = Category.objects.create(
+            name='flower',
+            friendly_name='Flower'
+        )
+
+        product = Product.objects.create(
+            name='Test',
+            species='species',
+            price=12.00,
+            planting_conditions='planting_conditions',
+            rating=0,
+            description='description',
+            category=category,
+
+        )
+
+        response = self.client.get(f'/products/{product.id}/')
+        self.assertEqual(response.status_code, 200)
+
+        template = 'products/product_detail.html'
+        self.assertTemplateUsed(response, template)
