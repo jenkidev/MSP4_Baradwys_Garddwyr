bebf65aac9fbe38eac5159dd7ce005d13d329ac4
jenkidev <>
2023-12-16 13:29:55 +0000

    Updated image field in article management

--- articles/forms.py
+++ articles/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Article
 
 
@@ -7,3 +8,11 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
