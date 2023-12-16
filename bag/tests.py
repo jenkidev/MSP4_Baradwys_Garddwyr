ed3d5fc4f62217687314f340221152309973a9e0
jenkidev <>
2023-12-16 20:59:00 +0000

    Moved bag test into bag folder

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_see_shopping_bag(self):
+        """ testing view for shopping bag """
+        response = self.client.get('/bag/')
+        self.assertEquals(response.status_code, 200)
+
+        template = 'bag/bag.html'
+        self.assertTemplateUsed(response, template)
--- checkout/tests.py
+++ checkout/tests.py
@@ -1,12 +1,3 @@
 from django.test import TestCase
 
-
-class TestViews(TestCase):
-
-    def test_see_shopping_bag(self):
-        """ testing view for shopping bag """
-        response = self.client.get('/bag/')
-        self.assertEquals(response.status_code, 200)
-
-        template = 'bag/bag.html'
-        self.assertTemplateUsed(response, template)
+# Create your tests here.
