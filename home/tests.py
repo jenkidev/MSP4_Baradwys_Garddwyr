09c3d2e62227f61f66c24a653fd525e32957f7b3
jenkidev <>
2023-12-16 20:56:17 +0000

    Created test for viewing home page

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_index(self):
+        """ testing view for displaying the home page """
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        template = 'home/index.html'
+
+        self.assertTemplateUsed(response, template)
