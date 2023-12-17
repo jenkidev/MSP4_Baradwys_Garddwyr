1490100861a684835376e9f4d4422e831a501881
jenkidev <>
2023-12-17 08:10:59 +0000

    setting up deployment

--- baradwys_garddwyr/settings.py
+++ baradwys_garddwyr/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-46p41*bqkly&%=t7n0oim_=wv@8jzz$9g(^94^$d6dmah__v--
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8005-jenkidev-msp4-baradwys-g-qy9tgqfz5s.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8005-jenkidev-msp4-baradwys-g-qy9tgqfz5s.us2.codeanyapp.com','baradwys-garddwyr.herokuapp.com']
 
 
 # Application definition
@@ -118,13 +119,20 @@ WSGI_APPLICATION = 'baradwys_garddwyr.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+
 
 
 # Password validation
