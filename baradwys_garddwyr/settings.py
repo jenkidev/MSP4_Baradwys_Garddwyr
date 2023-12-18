09acf6eaa7596497e78c20b1b49584e533b910b2
jenkidev <>
2023-12-18 21:47:35 +0000

    add hash to push to github

--- baradwys_garddwyr/settings.py
+++ baradwys_garddwyr/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!#
 SECRET_KEY = 'lrdcxlu_wz3@+e2%#vf@h@0^y52f$dd7q#apn3vxuhz*zyajhq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
