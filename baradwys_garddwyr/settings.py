4fbec334dac3f653bbaf2bf5db99d4f0d9f4bdb6
jenkidev <>
2023-12-18 21:10:35 +0000

    Update database in settings

--- baradwys_garddwyr/settings.py
+++ baradwys_garddwyr/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'baradwys_garddwyr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 if 'DATABASE_URL' in os.environ:
-DATABASES = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
@@ -130,7 +130,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
- }
+    }
 
 
 # Password validation
