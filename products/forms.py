f886b300d3366415ebb286305aaf9287d776576a
jenkidev <jenkimc@googlemail.com>
2024-02-18 16:02:25 +0000

    Added minimum value validation for price in product forms

--- products/forms.py
+++ products/forms.py
@@ -14,6 +14,7 @@ class ProductForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
+        self.fields['price'].widget.attrs['min'] = 0.01
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
