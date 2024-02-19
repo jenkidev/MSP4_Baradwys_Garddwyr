1424fa48aca0caa5abe868d05240ce85e85042a3
jenkidev <jenkimc@googlemail.com>
2024-02-19 17:19:38 +0000

    Created Test views for bag app

---
+++ bag/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_see_shopping_bag(self):
+        """ test view for shopping bag """
+        response = self.client.get('/bag/')
+        self.assertEqual(response.status_code, 200)
+
+        template = 'bag/bag.html'
+        self.assertTemplateUsed(response, template)
