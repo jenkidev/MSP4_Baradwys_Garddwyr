e262746212992d83fe7f5e5263e6bb9e0ec86d4b
jenkidev <jenkimc@googlemail.com>
2024-02-21 05:46:26 +0000

    Removed test-views file as was redundant, edited bag.tests syntax (assertEquals > assertEqual)

--- bag/tests.py
+++ bag/tests.py
@@ -6,7 +6,7 @@ class TestViews(TestCase):
     def test_see_shopping_bag(self):
         """ testing view for shopping bag """
         response = self.client.get('/bag/')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
         template = 'bag/bag.html'
         self.assertTemplateUsed(response, template)
