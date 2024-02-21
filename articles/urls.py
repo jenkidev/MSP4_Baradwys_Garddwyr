52f2648f3ad5589f4367622d29e287bc72455c3b
jenkidev <jenkimc@googlemail.com>
2024-02-21 06:26:20 +0000

    Refactor: Flake8 refactoring

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('<int:article_id>/', views.article_details, name='article_details'),
     path('add/', views.add_article, name='add_article'),
     path('edit/<int:article_id>/', views.edit_article, name='edit_article'),
-    path('delete/<int:article_id>/', views.delete_article, name='delete_article'),
+    path('delete/<int:article_id>/', views.delete_article,
+         name='delete_article'),
 ]
